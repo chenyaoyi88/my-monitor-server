9f57f9c9e49f74fa7440c1893aee6bfab22c7845
chenyaoyi88 <152296058@qq.com>
2018-03-07 17:59:06 +0800

    first-commit

---
+++ routes/msg_error.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/', function (req, res, next) {
+  const body = req.body;
+  console.log(body);
+  res.send({
+    code: '0000',
+    msg: 'success',
+    data: null
+  });
+});
+
+module.exports = router;
---
+++ routes/msg_http.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  const query = req.query;
+  console.log(query);
+  res.send({
+    code: '0000',
+    msg: 'success',
+    data: null
+  });
+});
+
+module.exports = router;
